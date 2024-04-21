36a6f6ecec9ca11d061a9c4d1d5f67be6d329ead
Lokesh-1234 <104615534+Lokesh-1234@users.noreply.github.com>
2024-04-21 16:52:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Add your JavaScript code here
+
+// Example: Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
